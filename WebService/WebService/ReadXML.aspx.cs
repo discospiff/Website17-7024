59e8e634af1ae19ac0266a4d1ed3b748d3da7e1b
jonesbr <brandan.jones@uc.edu>
2017-11-11 11:55:24 -0500

    Update codebehind, xsd, and xml

--- WebService/WebService/ReadXML.aspx.cs
+++ WebService/WebService/ReadXML.aspx.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace WebService
 {
     public partial class ReadXML : System.Web.UI.Page
     {
+        String fileName = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
+                
                 // we are hearing back from the page, after it was initially loaded.
 
                 // see if the user uploaded a document.
@@ -26,7 +31,7 @@ namespace WebService
                 //has the user uploaded a file, and is it a valid file.
                 if (XMLFileUpload.HasFile)
                 {
-                    String fileName = XMLFileUpload.FileName;
+                    
                     // now check the extension.
                     String fileExtension = System.IO.Path.GetExtension(fileName).ToLower();
 
@@ -36,7 +41,9 @@ namespace WebService
                         // construct the path where we want to save the file.
                         String path = Server.MapPath("~/XML/");
                         // if we are here, the file is valid, so save the file.
+
                         XMLFileUpload.PostedFile.SaveAs(path + XMLFileUpload.FileName);
+                        fileName = XMLFileUpload.FileName;
                         LblStatus.Text = "Upload Successful";
                     } else
                     {
@@ -48,7 +55,15 @@ namespace WebService
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            ValidateXML("");
+            if (fileName != null && fileName.Length > 0)
+            {
+                // declare an XML DOM document
+                XmlDocument doc = new XmlDocument();
+                doc.Load(fileName);
+
+                ValidateXML(fileName);
+            }
+            
         }
 
         /// <summary>
@@ -57,7 +72,36 @@ namespace WebService
         /// <param name="fileName">The XML file we want to validate.</param>
         private void ValidateXML(String fileName)
         {
+            // how we want to read and validate the document.
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
 
+            // what method do we call when things go wrong?
+            settings.ValidationEventHandler += new ValidationEventHandler(this.ValidationEventHandle);
+
+            // validate the document.
+            XmlReader xmlReader = XmlReader.Create(fileName, settings);
+            //read the document one line at a time.
+            while(xmlReader.Read())
+            {
+                // leave blank for the moment.
+
+            }
+            LblStatus.Text = "Validation Passed";
         }
+
+        /// <summary>
+        /// This method is called only when the XML fails validation.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ValidationEventHandle(object sender, ValidationEventArgs args)
+        {
+            Console.WriteLine("Vaidation error: " + args.Message);
+            LblStatus.Text = "Validation Failed.  Message: " + args.Message;
+        }
+
     }
 }
