a0924e51a5560955c89376df8eb735abce63a229
jonesbr <brandan.jones@uc.edu>
2017-11-11 10:06:52 -0500

    Start our web form.

---
+++ WebService/WebService/ReadXML.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReadXML.aspx.cs" Inherits="WebService.ReadXML" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Submit your Plant XML"></asp:Label>
+            <br />
+            <asp:FileUpload ID="XMLFileUpload" runat="server" />
+            <br />
+            <asp:Button ID="Button1" runat="server" CssClass="button" OnClick="Button1_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebService/WebService/ReadXML.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebService
+{
+    public partial class ReadXML : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ValidateXML("");
+        }
+
+        /// <summary>
+        /// Validate XML against an XSD.
+        /// </summary>
+        /// <param name="fileName">The XML file we want to validate.</param>
+        private void ValidateXML(String fileName)
+        {
+
+        }
+    }
+}
